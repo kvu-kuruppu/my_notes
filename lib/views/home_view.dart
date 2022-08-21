905571b7ec206689ce00ea549e3f20d26c6f7a37
Your Name <Your Email>
2022-08-22 02:22:03 +0530

    Log Out

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_notebook/firebase_options.dart';
 import 'package:my_notebook/views/login_view.dart';
+import 'package:my_notebook/views/notes_view.dart';
 import 'package:my_notebook/views/verify_email_view.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,16 +20,16 @@ class HomePage extends StatelessWidget {
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if(user != null) {
-                if(user.emailVerified) {
-                  print('Your email is verified');
+                final user = FirebaseAuth.instance.currentUser;
+                if (user != null) {
+                  if (user.emailVerified) {
+                    return const NotesView();
+                  } else {
+                    return const VerificationView();
+                  }
                 } else {
-                  return const VerificationView();
+                  return const LoginView();
                 }
-              } else{
-                return const LoginView();
-              }
               // print('hey $user');
               // if (user?.emailVerified ?? false) {
               // } else {
@@ -37,27 +38,6 @@ class HomePage extends StatelessWidget {
               //         builder: (context) => const VerificationView()));
               //   });
               // }
-              return ListView(
-                children: [
-                  Container(
-                    height: 100,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20,
-                    ),
-                    child: Row(
-                      children: const [
-                        Text(
-                          'Home',
-                          style: TextStyle(
-                              fontSize: 50, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Divider(),
-                  const Center(child: Text('Done')),
-                ],
-              );
               default:
                 return const Center(child: Text('Loading...'));
             }
@@ -65,4 +45,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/views/notes_view.dart
@@ -0,0 +1,96 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 185, 185, 185),
+      appBar: AppBar(
+        toolbarHeight: 90,
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogOut = await showLogOutDialog(context);
+                  if (shouldLogOut) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/login', (_) => false);
+                  }
+                  devtools.log(shouldLogOut.toString());
+                  break;
+              }
+            },
+            itemBuilder: ((context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Center(child: Text('Log out')),
+                ),
+              ];
+            }),
+            icon: const Icon(Icons.person, color: Colors.black),
+          )
+        ],
+        title: const Text(
+          'Home',
+          style: TextStyle(
+              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 185, 185, 185),
+      ),
+      body: ListView(children: [
+        Container(
+          height: 100,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Row(
+            children: const [
+              Text(
+                'Hey dude',
+                style: TextStyle(fontSize: 40),
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Sign Out'),
+          content: const Text('Are you really wanna log out?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('No')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Yes')),
+          ],
+        );
+      }).then((value) => value ?? false);
+}
