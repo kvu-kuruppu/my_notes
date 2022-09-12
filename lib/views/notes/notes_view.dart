d3aa7398815c1834b50d2857a678279f2f5dde87
Your Name <Your Email>
2022-09-12 17:31:23 +0530

    Creating New Notes

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -77,6 +77,7 @@ class _NotesViewState extends State<NotesView> {
       ),
       body: ListView(
         children: [
+          // + Add Note Button
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -119,6 +120,7 @@ class _NotesViewState extends State<NotesView> {
                               builder: (context, snapshot) {
                                 switch (snapshot.connectionState) {
                                   case ConnectionState.waiting:
+                                  case ConnectionState.active:
                                     return const Text(
                                         'Waiting for all notes...');
                                   default:
